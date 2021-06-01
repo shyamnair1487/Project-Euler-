3396e3a6037a699cc011631add18e2a0d4080661
shyam nair <shyamnair1487@gmail.com>
2021-06-01 10:33:15 +0800

    Added default argument to three_five_mult function

--- prob1.py
+++ prob1.py
@@ -4,9 +4,10 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
 
-def three_five_mult():
 
-	return sum([x for x in range(1000) if x % 3 == 0 or x % 5 == 0])
+def three_five_mult(limit=1000):
+
+	return sum([x for x in range(limit) if x % 3 == 0 or x % 5 == 0])
 
 
 def Main():
