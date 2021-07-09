d417cd4ada596b82871b997adaeedfd281474c97
shyam nair <shyamnair1487@gmail.com>
2021-07-09 11:31:55 +0800

    Added function to determine if num is palindrome

--- prob4.py
+++ prob4.py
@@ -6,3 +6,29 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
+
+
+
+
+# converting int to string then splitting in half 
+
+
+# determining if the two sides are the same
+
+def isPalindrome(num):
+
+	num = str(num)
+
+	hal_len = int(0.5* len(num))
+
+	if len(num) % 2 == 0:
+
+		return num[:hal_len] == num[half_len:]
+
+	else:
+
+		return num[:half_len] == num[(half_len+1):]
+
+
+
+# loop to find product of two 3 digit numbers, then determine if its a palindrome
