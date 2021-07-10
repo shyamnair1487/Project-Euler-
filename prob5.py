11be23bc2cf0deb14e05e509de22e92e47e5370a
shyam nair <shyamnair1487@gmail.com>
2021-07-10 09:02:58 +0800

    Starting again. Trying division method for LCM. Added function for recognising primes

--- prob5.py
+++ prob5.py
@@ -4,12 +4,33 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 """
 
-def divide_all(num):
+# def divide_all(num):
 
-	div_all = True
-	for i in range(1,11):
+# 	div_all = True
+# 	for i in range(1,21):
 
-		if num % i != 0:
-			div_all = False
+# 		if num % i != 0:
+# 			div_all = False
+# 			break
+# 	return div_all
+
+# x = 21
+# while True:
+
+# 	if divide_all(x):
+
+# 		break
+# 	else:
+# 		x += 1
+
+# print(x)
+
+def isPrime(num):
+
+	prime = True
+	for factor in range(2,num):
+
+		if num % factor == 0:
+			prime = False
 			break
-	return div_all
+	return prime
