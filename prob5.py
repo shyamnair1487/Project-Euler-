14e5a5825968f3eb91e1e91436a02eb525fff121
shyam nair <shyamnair1487@gmail.com>
2021-07-10 08:46:36 +0800

    Added function that checks if a number can be divided by all the numbers in a certain range without any remainder

--- prob5.py
+++ prob5.py
@@ -4,3 +4,12 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 """
 
+def divide_all(num):
+
+	div_all = True
+	for i in range(1,11):
+
+		if num % i != 0:
+			div_all = False
+			break
+	return div_all
