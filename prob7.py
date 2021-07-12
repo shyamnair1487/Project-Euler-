dabd9fa4bcb32619c41b96165796570e395e3dac
shyam nair <shyamnair1487@gmail.com>
2021-07-12 18:09:44 +0800

    Added argparse and math modules

--- prob7.py
+++ prob7.py
@@ -4,3 +4,14 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 What is the 10 001st prime number?
 
 """
+import argparse, math
+
+
+def Main():
+
+	pass
+
+
+if __name__ == "__main__":
+
+	Main()
