f6871d83520683ab723f9b96a865f36b624e9458
shyam nair <shyamnair1487@gmail.com>
2021-07-12 18:15:15 +0800

    Added function to determine if number is prime

--- prob7.py
+++ prob7.py
@@ -6,10 +6,34 @@ What is the 10 001st prime number?
 """
 import argparse, math
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument("n", type=int, help="where n is the nth prime number")
+
+args = parser.parse_args()
+
+def isPrime(num):
+    
+    square = math.ceil(math.sqrt(num))
+    if num < 1000:
+        square = num
+    else:
+        square += 1
+    prime = True 
+    for i in range(2,square):
+        if num % i == 0:
+            prime = False
+            break
+    return prime
+
 
 def Main():
 
-	pass
+	n = args.n
+	print(n)
+	for i in range(2,50):
+		if isPrime(i):
+			print(i, end=" ")
 
 
 if __name__ == "__main__":
