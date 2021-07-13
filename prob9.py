0c8b30eef6f7b4bc3b0f0148772e146447eaee27
shyam nair <shyamnair1487@gmail.com>
2021-07-13 09:27:30 +0800

    Looped through all possible combination to find pythagorean triplet

--- prob9.py
+++ prob9.py
@@ -10,7 +10,14 @@ Find the product abc.
 
 def Main():
 
-	pass
+	for i in range(1,500):
+		for j in range(1,500):
+			for k in range(1,500):
+				if i + j + k == 1000:
+
+					if i**2 + j**2 == k**2:
+
+						print(i*j*k)
 
 
 if __name__ == "__main__":
