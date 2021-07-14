44cbe322d3df7db14635e6a84dbbe6d56e30ff4d
shyam nair <shyamnair1487@gmail.com>
2021-07-14 09:18:32 +0800

    Added code skeleton and argparse

--- prob10.py
+++ prob10.py
@@ -2,4 +2,22 @@
 The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes below two million.
+
 """
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-n', "--num", default=2000000, type=int, help="number below which to find summation of all primes")
+
+args = parser.parse_args()
+
+def Main():
+
+	num = args.num
+	print(num)
+
+
+if __name__ == "__main__":
+
+	Main()
