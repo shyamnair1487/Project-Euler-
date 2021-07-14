91ea5129d543d7ff6843e9350d7e0948ff17ea17
shyam nair <shyamnair1487@gmail.com>
2021-07-14 09:19:26 +0800

    Added function to identify primes

--- prob10.py
+++ prob10.py
@@ -12,6 +12,21 @@ parser.add_argument('-n', "--num", default=2000000, type=int, help="number below
 
 args = parser.parse_args()
 
+def isPrime(num):
+    if num <= 1:
+        return False
+    
+    square = math.ceil(math.sqrt(num)) + 1
+    
+    if num < 1000:
+        square = num
+    else:
+        square += 1
+    for i in range(2,square):
+        if num % i == 0:
+            return False
+    return True
+
 def Main():
 
 	num = args.num
